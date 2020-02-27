379a5edfc34b886287a025a3c05283cdc125ecc3
Sion Pixley <sionpixley@gmail.com>
2020-02-26 19:13:31 -0600

    Got the views file setup. Haven't started implementation yet.

--- southeastcsapi/api/views.py
+++ southeastcsapi/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from api.models import announcement, event, article, contact
+from api.serializers import announcement_serializer, event_serializer, article_serializer, contact_serializer
 
-# Create your views here.
+
