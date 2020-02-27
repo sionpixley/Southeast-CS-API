3d0b38b0bf81ab9bf4e37d8ae16d6261bf40e95b
Sion Pixley <sionpixley@gmail.com>
2020-02-27 11:29:13 -0600

    Added add-admin endpoint and get-all-admins endpoint.

--- southeastcsapi/api/urls.py
+++ southeastcsapi/api/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 import api.views as views
 
 urlpatterns = [
+    path("add-admin/", views.add_admin),
+    path("get-all-admins/", views.get_all_admins)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- southeastcsapi/api/views.py
+++ southeastcsapi/api/views.py
@@ -6,4 +6,29 @@ from rest_framework import status
 from api.models import announcement, event, article, contact, admin
 from api.serializers import announcement_serializer, event_serializer, article_serializer, contact_serializer, admin_serializer
 
+@csrf_exempt
+def add_admin(request):
+    if request.method != "POST":
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
 
+    admin_dict = JSONParser().parse(request)
+
+    try:
+        check_admin = admin.objects.get(username=admin_dict["username"])
+        return HttpResponse(status=status.HTTP_302_FOUND)
+    except admin.DoesNotExist:
+        new_admin = admin_serializer(data=admin_dict)
+        if new_admin.is_valid():
+            new_admin.save()
+            return HttpResponse(status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def get_all_admins(request):
+    if request.method != "GET":
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+    admins = admin.objects.all().distinct()
+    admins_dict = admin_serializer(admins, many=True)
+    return JsonResponse(admins_dict.data, safe=False)
