78fd6f1416b56eae4ef2041a7c1ccd466f6eb7fc
Sion Pixley <sionpixley@gmail.com>
2020-02-29 12:54:19 -0600

    Fixed fields in serializers.py.

--- southeastcsapi/api/serializers.py
+++ southeastcsapi/api/serializers.py
@@ -5,19 +5,19 @@ from api.models import announcement, event, article, contact, admin
 class announcement_serializer(serializers.ModelSerializer):
     class Meta:
         model = announcement
-        fields = ("id", "author", "authored_date", "heading", "info")
+        fields = ("id", "author", "authored_date", "subject", "description")
 
 
 class event_serializer(serializers.ModelSerializer):
     class Meta:
         model = event
-        fields = ("id", "date", "location", "name", "info", "organization")
+        fields = ("id", "date", "location", "name", "description", "organization")
 
 
 class article_serializer(serializers.ModelSerializer):
     class Meta:
         model = article
-        fields = ("id", "heading", "info")
+        fields = ("id", "subject", "description")
 
 
 class contact_serializer(serializers.ModelSerializer):
